4d3001cde220f40c18bfe1527ab2a0b0a5e9906a
hridoy269 <146862058+hridoy269@users.noreply.github.com>
2023-10-03 09:49:42 -0700

    Add files via upload

---
+++ Domain_to_IP.py
@@ -0,0 +1,18 @@
+# Domain to ip
+
+from termcolor import colored
+
+print(colored("****************Domain To IP Convertor****************",'green'))
+print(colored("****************Create By Stallker(*_*)*****************",'red'))
+
+import pyfiglet # banner package
+import socket
+banner = colored(pyfiglet.figlet_format("IP Convertor"),'cyan') # use for banner
+print(banner)
+
+domain_Name = input("Enter You target Domain:")\
+
+ip = socket.gethostbyname(domain_Name)
+
+print("IP For {} : {}".format(domain_Name,ip))
+
